fca85b83b90dc660b53105b37b28e7888d058c5d
Abdallah Beshary <abdallahuuuio221@gmail.com>
2025-01-15 20:46:07 +0200

    edit

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
 import streamlit as st 
 import pickle as pkl 
-from helper import text_preprocessing
+from nltk.tokenize import word_tokenize
+import re
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle as pkl 
+
+
+with open('tf.pkl', 'rb') as file:
+    model = pkl.load(file)
+
+tf = TfidfVectorizer()
+stop_words = stopwords.words('english')
+stemmer = PorterStemmer()
+
+def text_preprocessing(text):
+  ## lower case
+  text = text.lower()
+  ## special charcter
+  text = re.sub('[^a-zA-z]', ' ', text)
+  ## Tokinzation
+  text = word_tokenize(text)
+  ## stopwords
+  text = [word for word in text if word not in stop_words]
+  ## lemmetization
+  text = [stemmer.stem(word) for word in text]
+  text = ' '.join(text)
+  text = model.transform([text])
+  return text
+
 
 with open('svc.pkl', 'rb') as file:
     model = pkl.load(file)
